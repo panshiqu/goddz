71513fdaba5544c2b7f9c49bc6f8a8df755a860d
张圣超 <zhangshengchao@zhangshengchaodeMacBook-Air.local>
2016-07-31 15:26:01 +0800

    instruction realize menu

--- logic/Player.go
+++ logic/Player.go
@@ -36,43 +36,43 @@ func (p *Player) OnEvent(message string) {
 
 	switch message {
 	// 狼羊菜过河
-	case "1001":
+	case "1001", "狼羊菜过河", "langyangcaiguohe":
 		p.game = new(Game1001)
 
 	// 三人三鬼过河
-	case "1002":
+	case "1002", "三人三鬼过河", "sanrensanguiguohe":
 		p.game = new(Game1002)
 
 	// 警犯一家人过河
-	case "1003":
+	case "1003", "警犯一家人过河", "jingfanyijiarenguohe":
 		p.game = new(Game1003)
 
 	// 一家人过独木桥
-	case "1004":
+	case "1004", "一家人过独木桥", "yijiarenguodumuqiao":
 		p.game = new(Game1004)
 
 	// 指挥电梯上下
-	case "1005":
+	case "1005", "指挥电梯上下", "zhihuidiantishangxia":
 		p.game = new(Game1005)
 
 	// 青蛙直线跳棋
-	case "1006":
+	case "1006", "青蛙直线跳棋", "qingwazhixiantiaoqi":
 		p.game = new(Game1006)
 
 	// 哥弟俩均分饮料
-	case "1007":
+	case "1007", "哥弟俩均分饮料", "gediliajunfenyinliao":
 		p.game = new(Game1007)
 
 	// 切分金条发工资
-	case "1008":
+	case "1008", "切分金条发工资", "qiefenjintiaofagongzi":
 		p.game = new(Game1008)
 
 	// 把灯全打开
-	case "1009":
+	case "1009", "把灯全打开", "badengquandakai":
 		p.game = new(Game1009)
 
 	// 智力测试图集
-	case "1010":
+	case "1010", "智力测试图集", "zhiliceshituji":
 		p.game = new(Game1010)
 
 	default:
@@ -84,27 +84,27 @@ func (p *Player) OnEvent(message string) {
 
 		switch message {
 		// 问题背景
-		case "qb":
+		case "qb", "问题背景", "wentibeijing":
 			wechat.PushTextMessage(p.openid, p.game.Background())
 			return
 
 		// 操作说明
-		case "oi":
+		case "oi", "操作说明", "caozuoshuoming":
 			wechat.PushTextMessage(p.openid, p.game.Description())
 			return
 
 		// 当前场景
-		case "cs":
+		case "cs", "当前场景", "dangqianchangjing":
 			wechat.PushTextMessage(p.openid, p.game.GameScene())
 			return
 
 		// 获取提示
-		case "tip":
+		case "tip", "获取提示", "huoqutishi":
 			wechat.PushTextMessage(p.openid, p.game.GameTips())
 			return
 
 		// 游戏攻略
-		case "strategy":
+		case "strategy", "游戏攻略", "youxigonglve":
 			if p.game.Strategy() == "" {
 				wechat.PushTextMessage(p.openid, "没有游戏攻略")
 			} else {
